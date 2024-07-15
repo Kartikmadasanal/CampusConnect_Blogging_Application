76029c9c73efb599021a607ebb159a36cfdb974d
Kartikmadasanal <117498824+Kartikmadasanal@users.noreply.github.com>
2024-07-16 02:46:31 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ console.log(err);
 const port = process.env.PORT || 3000 ;
 
 app.use(express.json())
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({origin:"https://campusconnect-clint.onrender.com",credentials:true}))
 app.use(cookieParser())
 
 app.use("/api/auth",authRoute)
